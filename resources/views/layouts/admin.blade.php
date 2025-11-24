c0fcc3fa13edbb74a115325abf4d3daa40a61923
Wency Jorda <wencyjorda7@gmail.com>
2025-11-24 14:26:34 +0800

    fix all bugs and errors on admin side

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,29 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $title ?? 'Admin Dashboard' }}</title>
-  <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Admin Dashboard' }}</title>
+
+    <!-- SB Admin 2 CSS -->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
+
 <body id="page-top">
 
-  <div id="wrapper">
-    @include('partials.sidebar') <!-- Sidebar menu -->
-    <div id="content-wrapper" class="d-flex flex-column">
-      <div id="content">
-        @include('partials.topbar') <!-- Top navigation bar -->
-        <div class="container-fluid">
-          @yield('content') <!-- Main content -->
-        </div>
-      </div>
-      @include('partials.footer') <!-- Optional footer -->
-    </div>
-  </div>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-  <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-  @stack('scripts')
+        {{-- ✅ UPDATED SIDEBAR HERE --}}
+        @include('partials.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                {{-- Topbar --}}
+                @include('partials.topbar')
+
+                <!-- Main Container -->
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+
+            </div>
+
+            {{-- Footer --}}
+            @include('partials.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- SB Admin 2 JS -->
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+    @stack('scripts')
+
 </body>
 </html>
